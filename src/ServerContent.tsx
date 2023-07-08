49372d8ec7bf9f310f68f852840adb7b06151589
BernardoDeveloper <bernardogualberto8@gmail.com>
2023-07-08 12:36:24 -0300

    fix: remove default generate files

--- src/ServerContent.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentChildren } from "preact";
-
-type Props = {
-  children: ComponentChildren;
-};
-
-export function ServerContent({ children }: Props) {
-  return <div>{children}</div>;
-}
